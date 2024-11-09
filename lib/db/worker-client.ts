70cdca5ad49f781ba1325d7062e63733a2ccee5d
Masaki Hara <ackie.h.gmai@gmail.com>
2024-11-09 22:40:02 +0900

    Impl more queries

--- lib/db/worker-client.ts
+++ lib/db/worker-client.ts
@@ -1,5 +1,5 @@
-import { QueryParameterSet, Row, SqliteOptions } from "sqlite";
-import { CloseRequest, ConnectRequest, QueryRequest, WorkerRequest, WorkerResponse } from "./worker-protocol.ts";
+import type { ColumnName, QueryParameterSet, Row, RowObject, SqliteDeserializeOptions, SqliteOptions } from "sqlite";
+import { CloseRequest, ConnectRequest, IteratorNextRequest, IteratorNextWithValueRequest, IteratorProfile, PrepareQueryRequest, QueryRequest, WorkerRequest, WorkerResponse, WorkerReturnValues, PreparedQueryIterRequest, PreparedQueryIterEntriesRequest, IteratorReturnWithValueRequest, IteratorReturnRequest, IteratorThrowRequest, QueryEntriesRequest, PreparedQueryAllRequest, PreparedQueryAllEntriesRequest, PreparedQueryFirstRequest, PreparedQueryFirstEntryRequest, PreparedQueryOneRequest, PreparedQueryOneEntryRequest, PreparedQueryExecuteRequest, PreparedQueryFinalizeRequest, PreparedQueryColumnsRequest, PreparedQueryExpandSqlRequest, ExecuteRequest, SerializeRequest, DeserializeRequest } from "./worker-protocol.ts";
 
 const enableWorkerPermissions = false;
 
@@ -8,10 +8,14 @@ type WorkerClientOptions = {
   writePermissions: URL[];
 };
 
+const AsyncIteratorPrototype = Object.getPrototypeOf(Object.getPrototypeOf(async function* () {}).prototype);
+
 class WorkerClient {
   #worker: Worker;
   #currentId = 1;
   #callbacks = new Map<number, (resp: WorkerResponse) => void>();
+  queryId = 1;
+  iterId = 1;
 
   constructor(options: WorkerClientOptions) {
     const workerOptions: WorkerOptions = { type: "module" };
@@ -34,14 +38,14 @@ class WorkerClient {
     };
   }
 
-  call(req: WorkerRequest): Promise<WorkerResponse> {
+  call<Req extends WorkerRequest>(req: Req): Promise<WorkerReturnValues[Req["type"]]> {
     req.id = this.#currentId++;
     return new Promise((resolve, reject) => {
       this.#callbacks.set(req.id, (resp) => {
         if (resp.type === "error") {
           reject(new Error(resp.message));
         } else {
-          resolve(resp);
+          resolve(resp.value as WorkerReturnValues[Req["type"]]);
         }
       });
       this.#worker.postMessage(req);
@@ -51,11 +55,47 @@ class WorkerClient {
   close(): void {
     this.#worker.terminate();
   }
+
+  // deno-lint-ignore no-explicit-any
+  createIterator<T, TReturn = any, TNext = any>(iterId: number, profile: IteratorProfile): AsyncIterableIterator<T, TReturn, TNext> {
+    // deno-lint-ignore no-this-alias
+    const client = this;
+    const iter: AsyncIterableIterator<T, TReturn, TNext> = Object.assign(Object.create(AsyncIteratorPrototype), {
+      async next(value?: unknown): Promise<IteratorResult<T, TReturn>> {
+        if (arguments.length > 0) {
+          return (await client.call(IteratorNextWithValueRequest(iterId, value))) as IteratorResult<T, TReturn>;
+        } else {
+          return (await client.call(IteratorNextRequest(iterId))) as IteratorResult<T, TReturn>;
+        }
+      },
+      async return(value?: TReturn | PromiseLike<TReturn>): Promise<IteratorResult<T, TReturn>> {
+        if (arguments.length > 0) {
+          const resolvedValue = await value;
+          return (await client.call(IteratorReturnWithValueRequest(iterId, resolvedValue))) as IteratorResult<T, TReturn>;
+        } else {
+          return (await client.call(IteratorReturnRequest(iterId))) as IteratorResult<T, TReturn>;
+        }
+      },
+      // deno-lint-ignore no-explicit-any
+      async throw(error?: any): Promise<IteratorResult<T, TReturn>> {
+        return (await client.call(IteratorThrowRequest(iterId, error))) as IteratorResult<T, TReturn>;
+      },
+    } satisfies Omit<AsyncIterableIterator<T, TReturn, TNext>, typeof Symbol.asyncIterator>);
+    if (!profile.hasReturn) {
+      delete iter.return;
+    }
+    if (!profile.hasThrow) {
+      delete iter.throw;
+    }
+    return iter;
+  }
 }
 
 export class DB {
   static #privateConstructorKey: unknown = {};
   #client: WorkerClient;
+  #queryId = 1;
+  #transactionDepth = 0;
 
   /**
    * Essentially private. Use {@link DB.connect} instead.
@@ -104,11 +144,52 @@ export class DB {
     sql: string,
     params: QueryParameterSet = {},
   ): Promise<R[]> {
-    const resp = await this.#client.call(QueryRequest(sql, params));
-    if (resp.type !== "rows") {
-      throw new Error(`Unexpected response type: ${resp.type}`);
+    return (await this.#client.call(QueryRequest(sql, params))) as R[];
+  }
+
+  async queryEntries<O extends RowObject = RowObject>(
+    sql: string,
+    params: QueryParameterSet = {},
+  ): Promise<O[]> {
+    return (await this.#client.call(QueryEntriesRequest(sql, params))) as O[];
+  }
+
+  async prepareQuery<
+    R extends Row = Row,
+    O extends RowObject = RowObject,
+    P extends QueryParameterSet = QueryParameterSet,
+  >(
+    sql: string,
+  ): Promise<PreparedQuery<R, O, P>> {
+    const queryId = this.#queryId++;
+    await this.#client.call(PrepareQueryRequest(sql, queryId));
+    return new PreparedQuery(this.#client, queryId, preparedQueryConstructorKey);
+  }
+
+  async execute(sql: string): Promise<void> {
+    await this.#client.call(ExecuteRequest(sql));
+  }
+
+  async transaction<V>(closure: () => Promise<V>): Promise<V> {
+    this.#transactionDepth += 1;
+    this.execute(`SAVEPOINT _deno_sqlite_async_sp_${this.#transactionDepth}`);
+    try {
+      return await closure();
+    } catch (err) {
+      this.execute(`ROLLBACK TO _deno_sqlite_async_sp_${this.#transactionDepth}`);
+      throw err;
+    } finally {
+      this.execute(`RELEASE _deno_sqlite_async_sp_${this.#transactionDepth}`);
+      this.#transactionDepth -= 1;
     }
-    return resp.rows as R[];
+  }
+
+  async serialize(schema: "main" | "temp" | string = "main"): Promise<Uint8Array> {
+    return await this.#client.call(SerializeRequest(schema));
+  }
+
+  async deserialize(data: Uint8Array, options: SqliteDeserializeOptions = {}): Promise<void> {
+    await this.#client.call(DeserializeRequest(data, options));
   }
 
   async close(force = false): Promise<void> {
@@ -117,5 +198,78 @@ export class DB {
     } finally {
       this.#client.close();
     }
+  }
+}
+
+const preparedQueryConstructorKey: unknown = {};
+
+export class PreparedQuery<
+  R extends Row = Row,
+  O extends RowObject = RowObject,
+  P extends QueryParameterSet = QueryParameterSet,
+> {
+  #client: WorkerClient;
+  #queryId: number;
+  /**
+   * Essentially private, do not call this outside of the library.
+   */
+  constructor(client: unknown, queryId: number, privateConstructorKey: unknown) {
+    if (privateConstructorKey !== preparedQueryConstructorKey) {
+      throw new Error("Use DB.prototype.prepareQuery() to create a new prepared query");
+    }
+    this.#client = client as WorkerClient;
+    this.#queryId = queryId;
+  }
+
+  async iter(params?: P): Promise<AsyncIterableIterator<R>> {
+    const iterId = this.#client.iterId++;
+    const profile = await this.#client.call(PreparedQueryIterRequest(this.#queryId, params, iterId));
+    return this.#client.createIterator<R>(iterId, profile);
+  }
+
+  async iterEntries(params?: P): Promise<AsyncIterableIterator<O>> {
+    const iterId = this.#client.iterId++;
+    const profile = await this.#client.call(PreparedQueryIterEntriesRequest(this.#queryId, params, iterId));
+    return this.#client.createIterator<O>(iterId, profile);
+  }
+
+  async all(params?: P): Promise<R[]> {
+    return (await this.#client.call(PreparedQueryAllRequest(this.#queryId, params))) as R[];
+  }
+
+  async allEntries(params?: P): Promise<O[]> {
+    return (await this.#client.call(PreparedQueryAllEntriesRequest(this.#queryId, params))) as O[];
+  }
+
+  async first(params?: P): Promise<R | undefined> {
+    return (await this.#client.call(PreparedQueryFirstRequest(this.#queryId, params))) as R | undefined;
+  }
+
+  async firstEntry(params?: P): Promise<O | undefined> {
+    return (await this.#client.call(PreparedQueryFirstEntryRequest(this.#queryId, params))) as O | undefined;
+  }
+
+  async one(params?: P): Promise<R> {
+    return (await this.#client.call(PreparedQueryOneRequest(this.#queryId, params))) as R;
+  }
+
+  async oneEntry(params?: P): Promise<O> {
+    return (await this.#client.call(PreparedQueryOneEntryRequest(this.#queryId, params))) as O;
+  }
+
+  async execute(params?: P): Promise<void> {
+    await this.#client.call(PreparedQueryExecuteRequest(this.#queryId, params));
+  }
+
+  async finalize(): Promise<void> {
+    await this.#client.call(PreparedQueryFinalizeRequest(this.#queryId));
+  }
+
+  async columns(): Promise<ColumnName[]> {
+    return await this.#client.call(PreparedQueryColumnsRequest(this.#queryId))
+  }
+
+  async expandSql(params?: P): Promise<string> {
+    return await this.#client.call(PreparedQueryExpandSqlRequest(this.#queryId, params))
   }
 }
--- lib/db/worker-protocol.ts
+++ lib/db/worker-protocol.ts
@@ -1,6 +1,59 @@
-import { QueryParameterSet, Row, RowObject } from "sqlite";
+import { ColumnName, QueryParameterSet, Row, RowObject, SqliteDeserializeOptions } from "sqlite";
 
-export type WorkerRequest = ConnectRequest | CloseRequest | QueryRequest | QueryEntriesRequest;
+export type WorkerRequest =
+  | ConnectRequest
+  | CloseRequest
+  | QueryRequest
+  | QueryEntriesRequest
+  | PrepareQueryRequest
+  | ExecuteRequest
+  | SerializeRequest
+  | DeserializeRequest
+  | PreparedQueryIterRequest
+  | PreparedQueryIterEntriesRequest
+  | PreparedQueryAllRequest
+  | PreparedQueryAllEntriesRequest
+  | PreparedQueryFirstRequest
+  | PreparedQueryFirstEntryRequest
+  | PreparedQueryOneRequest
+  | PreparedQueryOneEntryRequest
+  | PreparedQueryExecuteRequest
+  | PreparedQueryFinalizeRequest
+  | PreparedQueryColumnsRequest
+  | PreparedQueryExpandSqlRequest
+  | IteratorNextRequest
+  | IteratorNextWithValueRequest
+  | IteratorReturnRequest
+  | IteratorReturnWithValueRequest
+  | IteratorThrowRequest;
+
+export type WorkerReturnValues = {
+  "connect": void;
+  "close": void;
+  "query": Row[];
+  "query-entries": RowObject[];
+  "prepare-query": void;
+  "execute": void;
+  "serialize": Uint8Array;
+  "deserialize": void;
+  "prepared-query-iter": IteratorProfile;
+  "prepared-query-iter-entries": IteratorProfile;
+  "prepared-query-all": Row[];
+  "prepared-query-all-entries": RowObject[];
+  "prepared-query-first": Row | undefined;
+  "prepared-query-first-entry": RowObject | undefined;
+  "prepared-query-one": Row | undefined;
+  "prepared-query-one-entry": RowObject | undefined;
+  "prepared-query-execute": void;
+  "prepared-query-finalize": void;
+  "prepared-query-columns": ColumnName[];
+  "prepared-query-expand-sql": string;
+  "iterator-next": IteratorResult<unknown>;
+  "iterator-next-with-value": IteratorResult<unknown>;
+  "iterator-return": IteratorResult<unknown>;
+  "iterator-return-with-value": IteratorResult<unknown>;
+  "iterator-throw": IteratorResult<unknown>;
+};
 
 export type ConnectRequest = {
   type: "connect";
@@ -42,18 +95,204 @@ export type QueryEntriesRequest = {
   id: number;
   sql: string;
   params: QueryParameterSet;
-}
+};
 export function QueryEntriesRequest(sql: string, params: QueryParameterSet): QueryEntriesRequest {
   return { type: "query-entries", id: 0, sql, params };
 }
-
-export type WorkerResponse = VoidResponse | ErrorResponse | RowsResponse | RowObjectsResponse;
-export type VoidResponse = {
-  type: "void";
+export type PrepareQueryRequest = {
+  type: "prepare-query";
   id: number;
+  sql: string;
+  queryId: number;
 };
-export function VoidResponse(): VoidResponse {
-  return { type: "void", id: 0 };
+export function PrepareQueryRequest(sql: string, queryId: number): PrepareQueryRequest {
+  return { type: "prepare-query", id: 0, sql, queryId };
+}
+export type ExecuteRequest = {
+  type: "execute";
+  id: number;
+  sql: string;
+};
+export function ExecuteRequest(sql: string): ExecuteRequest {
+  return { type: "execute", id: 0, sql };
+}
+export type SerializeRequest = {
+  type: "serialize";
+  id: number;
+  schema: string;
+};
+export function SerializeRequest(schema: string): SerializeRequest {
+  return { type: "serialize", id: 0, schema };
+}
+export type DeserializeRequest = {
+  type: "deserialize";
+  id: number;
+  data: Uint8Array;
+  options: SqliteDeserializeOptions;
+};
+export function DeserializeRequest(data: Uint8Array, options: SqliteDeserializeOptions): DeserializeRequest {
+  return { type: "deserialize", id: 0, data, options };
+}
+export type PreparedQueryIterRequest = {
+  type: "prepared-query-iter";
+  id: number;
+  queryId: number;
+  params: QueryParameterSet | undefined;
+  iterId: number;
+};
+export function PreparedQueryIterRequest(queryId: number, params: QueryParameterSet | undefined, iterId: number): PreparedQueryIterRequest {
+  return { type: "prepared-query-iter", id: 0, queryId, params, iterId };
+}
+export type PreparedQueryIterEntriesRequest = {
+  type: "prepared-query-iter-entries";
+  id: number;
+  queryId: number;
+  params: QueryParameterSet | undefined;
+  iterId: number;
+};
+export function PreparedQueryIterEntriesRequest(queryId: number, params: QueryParameterSet | undefined, iterId: number): PreparedQueryIterEntriesRequest {
+  return { type: "prepared-query-iter-entries", id: 0, queryId, params, iterId };
+}
+export type PreparedQueryAllRequest = {
+  type: "prepared-query-all";
+  id: number;
+  queryId: number;
+  params: QueryParameterSet | undefined;
+};
+export function PreparedQueryAllRequest(queryId: number, params: QueryParameterSet | undefined): PreparedQueryAllRequest {
+  return { type: "prepared-query-all", id: 0, queryId, params };
+}
+export type PreparedQueryAllEntriesRequest = {
+  type: "prepared-query-all-entries";
+  id: number;
+  queryId: number;
+  params: QueryParameterSet | undefined;
+};
+export function PreparedQueryAllEntriesRequest(queryId: number, params: QueryParameterSet | undefined): PreparedQueryAllEntriesRequest {
+  return { type: "prepared-query-all-entries", id: 0, queryId, params };
+}
+export type PreparedQueryFirstRequest = {
+  type: "prepared-query-first";
+  id: number;
+  queryId: number;
+  params: QueryParameterSet | undefined;
+};
+export function PreparedQueryFirstRequest(queryId: number, params: QueryParameterSet | undefined): PreparedQueryFirstRequest {
+  return { type: "prepared-query-first", id: 0, queryId, params };
+}
+export type PreparedQueryFirstEntryRequest = {
+  type: "prepared-query-first-entry";
+  id: number;
+  queryId: number;
+  params: QueryParameterSet | undefined;
+};
+export function PreparedQueryFirstEntryRequest(queryId: number, params: QueryParameterSet | undefined): PreparedQueryFirstEntryRequest {
+  return { type: "prepared-query-first-entry", id: 0, queryId, params };
+}
+export type PreparedQueryOneRequest = {
+  type: "prepared-query-one";
+  id: number;
+  queryId: number;
+  params: QueryParameterSet | undefined;
+};
+export function PreparedQueryOneRequest(queryId: number, params: QueryParameterSet | undefined): PreparedQueryOneRequest {
+  return { type: "prepared-query-one", id: 0, queryId, params };
+}
+export type PreparedQueryOneEntryRequest = {
+  type: "prepared-query-one-entry";
+  id: number;
+  queryId: number;
+  params: QueryParameterSet | undefined;
+};
+export function PreparedQueryOneEntryRequest(queryId: number, params: QueryParameterSet | undefined): PreparedQueryOneEntryRequest {
+  return { type: "prepared-query-one-entry", id: 0, queryId, params };
+}
+export type PreparedQueryExecuteRequest = {
+  type: "prepared-query-execute";
+  id: number;
+  queryId: number;
+  params: QueryParameterSet | undefined;
+};
+export function PreparedQueryExecuteRequest(queryId: number, params: QueryParameterSet | undefined): PreparedQueryExecuteRequest {
+  return { type: "prepared-query-execute", id: 0, queryId, params };
+}
+export type PreparedQueryFinalizeRequest = {
+  type: "prepared-query-finalize";
+  id: number;
+  queryId: number;
+};
+export function PreparedQueryFinalizeRequest(queryId: number): PreparedQueryFinalizeRequest {
+  return { type: "prepared-query-finalize", id: 0, queryId };
+}
+export type PreparedQueryColumnsRequest = {
+  type: "prepared-query-columns";
+  id: number;
+  queryId: number;
+};
+export function PreparedQueryColumnsRequest(queryId: number): PreparedQueryColumnsRequest {
+  return { type: "prepared-query-columns", id: 0, queryId };
+}
+export type PreparedQueryExpandSqlRequest = {
+  type: "prepared-query-expand-sql";
+  id: number;
+  queryId: number;
+  params: QueryParameterSet | undefined;
+};
+export function PreparedQueryExpandSqlRequest(queryId: number, params: QueryParameterSet | undefined): PreparedQueryExpandSqlRequest {
+  return { type: "prepared-query-expand-sql", id: 0, queryId, params };
+}
+export type IteratorNextRequest = {
+  type: "iterator-next";
+  id: number;
+  iterId: number;
+};
+export function IteratorNextRequest(iterId: number): IteratorNextRequest {
+  return { type: "iterator-next", id: 0, iterId };
+}
+export type IteratorNextWithValueRequest = {
+  type: "iterator-next-with-value";
+  id: number;
+  iterId: number;
+  value: unknown;
+};
+export function IteratorNextWithValueRequest(iterId: number, value: unknown): IteratorNextWithValueRequest {
+  return { type: "iterator-next-with-value", id: 0, iterId, value };
+}
+export type IteratorReturnRequest = {
+  type: "iterator-return";
+  id: number;
+  iterId: number;
+};
+export function IteratorReturnRequest(iterId: number): IteratorReturnRequest {
+  return { type: "iterator-return", id: 0, iterId };
+}
+export type IteratorReturnWithValueRequest = {
+  type: "iterator-return-with-value";
+  id: number;
+  iterId: number;
+  value: unknown;
+};
+export function IteratorReturnWithValueRequest(iterId: number, value: unknown): IteratorReturnWithValueRequest {
+  return { type: "iterator-return-with-value", id: 0, iterId, value };
+}
+export type IteratorThrowRequest = {
+  type: "iterator-throw";
+  id: number;
+  iterId: number;
+  error: unknown;
+};
+export function IteratorThrowRequest(iterId: number, error: unknown): IteratorThrowRequest {
+  return { type: "iterator-throw", id: 0, iterId, error };
+}
+
+export type WorkerResponse = ValueResponse | ErrorResponse;
+export type ValueResponse = {
+  type: "value";
+  id: number;
+  value: unknown;
+}
+export function ValueResponse(value: unknown): ValueResponse {
+  return { type: "value", id: 0, value };
 }
 export type ErrorResponse = {
   type: "error";
@@ -63,19 +302,8 @@ export type ErrorResponse = {
 export function ErrorResponse(message: string): ErrorResponse {
   return { type: "error", id: 0, message };
 }
-export type RowsResponse = {
-  type: "rows";
-  id: number;
-  rows: Row[];
-}
-export function RowsResponse(rows: Row[]): RowsResponse {
-  return { type: "rows", id: 0, rows };
-}
-export type RowObjectsResponse = {
-  type: "row-objects";
-  id: number;
-  rows: RowObject[];
-}
-export function RowObjectsResponse(rows: RowObject[]): RowObjectsResponse {
-  return { type: "row-objects", id: 0, rows };
+
+export type IteratorProfile = {
+  hasReturn: boolean;
+  hasThrow: boolean;
 }
